9f01fa2156108ce5772f1ecc46b6e3ed7a6495df
Daniel E. Cook <Danielecook@gmail.com>
2017-02-24 08:07:07 -0600

    updates

--- process_concordance.R
+++ process_concordance.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript --vanilla
 library(tidyverse)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Used in calculating isotypes
 stack_list <- function(x) {
   if (is.null(names(x)) == T) {
@@ -8,37 +9,35 @@ stack_list <- function(x) {
   stack(x)
 }
 
-coverage_20 <- (readr::read_tsv("SM_coverage.tsv") %>%
+coverage_20 <- (readr::read_tsv("../SM/SM_coverage.tsv") %>%
                   dplyr::filter(coverage > 20))$strain
 
+WI <- readr::read_tsv("https://docs.google.com/spreadsheets/d/1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI/pub?output=tsv") 
 
-WI_strain_info <- readr::read_tsv("https://docs.google.com/spreadsheets/d/1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI/pub?output=tsv") %>%
-  dplyr::select(strain, isotype) %>%
-  dplyr::filter(!is.na(isotype)) %>%
-  dplyr::filter(strain %in% coverage_20)
+existing_WI <- WI %>%
+  dplyr::select(strain, isotype, latitude, longitude) %>%
+  dplyr::filter(strain %in% coverage_20) %>%
+  dplyr::filter(isotype != "NA")
 
-# Get number of Sites in VCF
+f <- file("isotype_count.txt")
 
-nsites = system("cat filtered.stats.txt  | grep 'SN.*number of SNPs' | cut -f 4", intern = T)
-nsites = as.numeric(nsites)
-
+cutoff <- 0.999
 
 gtcheck <- readr::read_tsv("gtcheck.tsv") %>%
   dplyr::filter((i %in% coverage_20) & (j %in% coverage_20)) %>%
-  dplyr::mutate(concordance = (nsites - discordance) / nsites) %>%
-  dplyr::mutate(isotype = concordance > 0.9993) 
-  
-#
-# Handle single strains
-#
+  dplyr::mutate(concordance = 1-(discordance/sites)) %>%
+  dplyr::mutate(isotype = concordance > cutoff) 
 
+# Generate strains that do not group with any other strains (single strains)
 single_strains <- gtcheck %>%
   dplyr::select(i, j, isotype) %>%
   tidyr::gather(col, strain, -isotype) %>%
   dplyr::group_by(strain) %>%
-  dplyr::mutate(single_strain = (sum(isotype) == 0)) %>%
+  dplyr::mutate(single_strain = (sum(isotype, na.rm = T) == 0)) %>%
   dplyr::distinct(.keep_all = T) %>%
-  dplyr::select(strain, single_strain) %>% dplyr::filter(single_strain)
+  dplyr::select(strain, single_strain) %>% 
+  dplyr::filter(single_strain)
+
 # Add LSJ1
 single_strains <- dplyr::bind_rows(list(strain = "LSJ1", single_strain = T), single_strains)
 
@@ -48,13 +47,15 @@ single_strains <- as.data.frame(list(i = single_strains$strain,
                                      concordance = 1,
                                      isotype = TRUE))
 
-
 gtcheck <- dplyr::bind_rows(gtcheck, single_strains)
 
+# Filter for only grouped strains
 iso_gtcheck <- gtcheck %>% dplyr::filter(isotype == T)
 
+# Generate complete strain list
 strain_list <- sort(unique(c(gtcheck$i, gtcheck$j)))
 
+# Generate isotype groups
 isotype_groups <- stack_list(unique(lapply(strain_list, function(x) {
   grouped_strains <- dplyr::filter(iso_gtcheck, (i == x | j == x)) %>%
     dplyr::select(i, j)
@@ -67,34 +68,62 @@ isotype_groups <- stack_list(unique(lapply(strain_list, function(x) {
   dplyr::distinct(data, .keep_all = T) %>%
   tidyr::unnest()
 
-isotype_groups <- dplyr::left_join(isotype_groups, WI_strain_info) %>%
-  dplyr::left_join(readr::read_tsv("SM_coverage.tsv")) %>%
+SM_coverage <- readr::read_tsv("../SM/SM_coverage.tsv")
+
+isotype_groups <- dplyr::left_join(isotype_groups, existing_WI, by = c("strain")) %>%
+  dplyr::left_join(SM_coverage) %>%
   dplyr::mutate(group = as.integer(group)) %>%
   dplyr::group_by(group) %>%
-  dplyr::mutate(unique_isotypes_per_group = length(unique(purrr::discard(isotype, is.na )))) %>%
-  dplyr::ungroup(isotype_groups) %>%
+  dplyr::mutate(avg_lat = abs(latitude - mean(latitude, na.rm = T)), avg_lon = abs(longitude - mean(longitude, na.rm = T))) %>%
+  dplyr::mutate(unique_isotypes_per_group = length(unique(purrr::discard(isotype, is.na)))) %>%
+  dplyr::group_by(isotype) %>%
+  dplyr::mutate(isotype_in_multiple_groups = unique_isotypes_per_group > 1) %>%
   dplyr::group_by(strain) %>%
-  dplyr::mutate(strain_conflicts = length(strain) != 1) 
+  dplyr::mutate(strain_in_multiple_isotypes = length(strain) > 1) %>%
+  dplyr::mutate(location_issue = (avg_lat > 5 | avg_lon > 5)) %>%
+  dplyr::select(-avg_lat,-avg_lon) %>%
+  dplyr::group_by(group) %>%
+  dplyr::mutate(strain_conflict = any(strain_in_multiple_isotypes,
+                                      location_issue,
+                                      isotype_in_multiple_groups,
+                                      unique_isotypes_per_group > 1,
+                                      na.rm = T))
 
 ggplot(gtcheck) +
   geom_histogram(aes(x=concordance, fill = isotype), binwidth = 0.00025) +
   scale_fill_manual(values = c("#808080", "#0080FF"))
 
-ggsave("concordance.svg")
-ggsave("concordance.png")
+ggsave("concordance.svg", width = 5, height = 5)
+ggsave("concordance.png", width = 5, height = 5)
 
 ggplot(gtcheck) +
   geom_histogram(aes(x=concordance, fill = isotype), binwidth = 0.000025) +
   scale_fill_manual(values = c("#808080", "#0080FF")) +
   scale_x_continuous(limits = c(0.99, 1.0)) +
   labs(x = "Concordance", y = "Number of Comparisons") +
-  geom_vline(aes(xintercept = 0.9993), color = "red") +
+  geom_vline(aes(xintercept = cutoff), color = "red") +
   theme(axis.title = ggplot2::element_text(size=14, face="bold", color="black", vjust=5))
 
-ggsave("xconcordance.svg")
-ggsave("xconcordance.png")
+ggsave("xconcordance.svg", width = 5, height = 5)
+ggsave("xconcordance.png", width = 5, height = 5)
 
 # Save text files
-readr::write_tsv(gtcheck, "gtcheck.tsv")
-readr::write_tsv(isotype_groups, "isotype_groups.tsv")
+readr::write_tsv(gtcheck, paste0("gtcheck.tsv"))
+readr::write_tsv(isotype_groups, paste0("isotype_groups.tsv"))
 
+
+# Output problem strains
+pr_strains <- unique((isotype_groups %>% 
+                        dplyr::filter(strain_conflict))$strain)
+
+pr_strain_comparison <- gtcheck %>% dplyr::filter(i %in% pr_strains, j %in% pr_strains) %>% 
+  dplyr::filter(discordance < 10000) %>% 
+  dplyr::left_join(existing_WI %>% dplyr::rename(iso_i = isotype) %>% dplyr::select(-latitude, -longitude), by = c("i"= "strain")) %>%
+  dplyr::left_join(existing_WI %>% dplyr::rename(iso_j = isotype) %>% dplyr::select(-latitude, -longitude), by = c("j"= "strain")) %>%
+  dplyr::filter(!is.na(sites))
+
+readr::write_tsv(pr_strain_comparison, paste0("problem_strains.tsv"))
+
+isotypes <- length(unique(isotype_groups$group))
+
+writeLines(paste0(isotypes, sep = "\n"), f)
