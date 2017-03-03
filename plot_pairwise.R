17cf5e6e0c63bcca2c9c87bab2e9901e2d2cabc2
Daniel Cook <danielecook@gmail.com>
2017-03-03 00:21:03 -0600

    Set max for easy comparison

--- plot_pairwise.R
+++ plot_pairwise.R
@@ -4,10 +4,17 @@ df <- readr::read_tsv("out.tsv", col_names = c("CHROM", "POS", "PAIR")) %>%
       dplyr::group_by(CHROM,bin) %>%
       dplyr::summarize(count=n())
 
+if (max(df$count) > 250) {
+    set_max = NA
+else {
+    set_max = 250
+}
+
 ggplot(df, aes(x = bin, y = count)) + 
   geom_bar(aes(fill = count >= 50), stat='identity') +
   scale_fill_manual(values = c("gray", "red")) +
   facet_grid(. ~ CHROM, scales = "free_x") +
-  theme_bw() 
+  theme_bw()  +
+  scale_y_continuous(limits = c(0, set_max))
 
 ggsave("out.png", width = 12, height = 3)
